82349670468af193dddce48484a4c69271f1b7ec
fabio-perezz <fabio_perez@hakkoda.io>
2022-03-28 13:17:43 -0600

    rm cached files to refresh git ignore

--- target/compiled/usfbm_data_mart/models/dimensions/DIM_WORK_CATEGORY.sql
+++
@@ -1,7 +0,0 @@
-WITH source -- the CTE view name
-	AS(
-        SELECT *
-        FROM GOLD.RED_PRAIRIE.WORKCATEGORY
-        WHERE WCT_NAME != '<none>' LIMIT 100
-    )
-SELECT * FROM source -- from the CTE view build a new reference with this filename
