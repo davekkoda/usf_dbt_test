1df8d76b492367997e4ca4452c3e77f13ae5ca42
fabio-perezz <fabio_perez@hakkoda.io>
2022-03-28 13:17:43 -0600

    rm cached files to refresh git ignore

--- models/dimensions/DIM_WORK_CATEGORY.sql
+++ models/dimensions/DIM_WORK_CATEGORY.sql
@@ -1,9 +1,7 @@
-{% set nm_tbl = selcet_dev_db('GOLD_RED_PRAIRIE') %} -- name of raw table
-
 WITH source -- the CTE view name
 	AS(
         SELECT *
-        FROM {{ source('{{ nm_tbl }}', 'WORKCATEGORY') }}
+        FROM {{ source('GOLD_RED_PRAIRIE', 'WORKCATEGORY') }}
         WHERE WCT_NAME != '<none>' LIMIT 100
     )
 SELECT * FROM source -- from the CTE view build a new reference with this filename
