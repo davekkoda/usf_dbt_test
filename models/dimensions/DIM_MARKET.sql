c0e540593c8f366a50562f716534530fae7c7681
fabio-perezz <fabio_perez@hakkoda.io>
2022-03-29 16:31:42 -0600

    QA CICD testing

--- models/dimensions/DIM_MARKET.sql
+++ models/dimensions/DIM_MARKET.sql
@@ -3,6 +3,7 @@ WITH source -- the CTE view name
         SELECT
             {{ dbt_utils.surrogate_key(['WH_ID']) }} AS DIM_MARKET_SK
             , *
+            
         FROM {{ ref('INT_DIM_MARKET') }}
     )
 SELECT * FROM source -- from the CTE view build a new reference with this filename
