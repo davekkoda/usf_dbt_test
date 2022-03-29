7fe067e2098026bec367c98e15d9426469b33ad1
fabio-perezz <fabio_perez@hakkoda.io>
2022-03-29 16:50:27 -0600

    QA CICD testing

--- models/dimensions/DIM_DRIVER.sql
+++ models/dimensions/DIM_DRIVER.sql
@@ -3,6 +3,7 @@ WITH source -- the CTE view name
         SELECT 
             {{ dbt_utils.surrogate_key(['WH_ID', 'RPRT_DT_TM']) }} AS DIM_DRIVER_SK
             , *
+            
         FROM {{ ref('INT_DIM_DRIVER') }}
     )
 SELECT * FROM source -- from the CTE view build a new reference with this filename
