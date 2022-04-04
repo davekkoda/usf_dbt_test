24bd7f4a6f425cf8adb8af435648c8006cf380ba
fabio-perezz <fabio_perez@hakkoda.io>
2022-04-04 15:44:10 -0600

    first version of icremental table for fact model

--- models/facts/FCT_ASSIGMENT.sql
+++ models/facts/FCT_ASSIGMENT.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='incremental') }}
+
 WITH source -- the CTE view name
 	AS(
         SELECT SM.WCT_ID
@@ -16,10 +18,19 @@ WITH source -- the CTE view name
         ,SM.START_TIME
         ,SM.STOP_TIME
         ,ADJ.ADJ_DURATION 
+<<<<<<< HEAD
         ,{{ dbt_utils.surrogate_key(['SM.WH_CD', 'SM.WCT_ID', 'SM.SRC_ID']) }} AS DIM_WORK_CATEGORY_SK
         ,{{ dbt_utils.surrogate_key(['SM.WH_CD']) }} AS DIM_MARKET_SK
         ,{{ dbt_utils.surrogate_key(['SM.WH_CD', 'SM.JOBCODE_ID', 'SM.SRC_ID']) }} AS DIM_JOBCODE_SK
         ,{{ dbt_utils.surrogate_key(['SM.WH_CD', 'SM.REPORT_DATE']) }} AS DIM_DRIVER_SK
+=======
+        ,ADJ.ADJ_INT_ID
+        ,SM.LAST_UPD_DT
+        ,{{ dbt_utils.surrogate_key(['SM.WH_ID', 'SM.WCT_ID', 'SM.SRC_ID']) }} AS DIM_WORK_CATEGORY_SK
+        ,{{ dbt_utils.surrogate_key(['SM.WH_ID']) }} AS DIM_MARKET_SK
+        ,{{ dbt_utils.surrogate_key(['SM.WH_ID', 'SM.JOBCODE_ID', 'SM.SRC_ID']) }} AS DIM_JOBCODE_SK
+        ,{{ dbt_utils.surrogate_key(['SM.WH_ID', 'SM.REPORT_DATE']) }} AS DIM_DRIVER_SK
+>>>>>>> a8fa696 (first version of icremental table for fact model)
         ,TO_NUMBER(TO_CHAR(TO_DATE(SM.REPORT_DATE),'YYYYMMDD'))AS DIM_DATE_SK
         FROM {{ ref( 'INT_KVI_SUMMARY') }} AS SM
         LEFT OUTER JOIN {{ ref('INT_KVI_ADJUSTMENTS') }} AS ADJ
@@ -28,5 +39,9 @@ WITH source -- the CTE view name
         AND   SM.WCT_ID = ADJ.WCT_ID
         AND   SM.KVISUMMARY_ID = ADJ.KVISUMMARY_ID
         AND   SM.SRC_ID = ADJ.SRC_ID
+
+        {% if is_incremental() %}
+            WHERE LAST_UPD_DT > (SELECT MAX(LAST_UPD_DT) from {{ this }})
+        {% endif %}
     )
 SELECT * FROM source -- from the CTE view build a new reference with this filename
