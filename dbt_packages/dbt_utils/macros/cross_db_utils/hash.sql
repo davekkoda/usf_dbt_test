83c735b13feea7cacea8ae459cde3c0007d809d6
fabio-perezz <fabio_perez@hakkoda.io>
2022-04-05 11:16:37 -0600

    Incremental CICD testing

--- dbt_packages/dbt_utils/macros/cross_db_utils/hash.sql
+++ dbt_packages/dbt_utils/macros/cross_db_utils/hash.sql
@@ -2,10 +2,9 @@
   {{ return(adapter.dispatch('hash', 'dbt_utils') (field)) }}
 {%- endmacro %}
 
--- EDITED TO USE MD5_NUMBER_UPPER64 and not MD5
 
 {% macro default__hash(field) -%}
-    MD5_NUMBER_UPPER64(cast({{field}} as {{dbt_utils.type_string()}}))
+    md5(cast({{field}} as {{dbt_utils.type_string()}}))
 {%- endmacro %}
 
 
