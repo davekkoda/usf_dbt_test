66e85a44a3f990b3013bd6deed9a4a0ae29a725d
fabio-perezz <fabio_perez@hakkoda.io>
2022-04-06 10:46:32 -0600

    working Great Expectations, first version

--- dbt_packages/spark_utils/macros/dbt_utils/cross_db_utils/split_part.sql
+++
@@ -1,8 +0,0 @@
-{% macro spark__split_part(string_text, delimiter_text, part_number) %}
-
-    split(
-        {{ string_text }},
-        concat('\\', {{ delimiter_text }})
-        )[({{ part_number - 1 }})]
-
-{% endmacro %}
