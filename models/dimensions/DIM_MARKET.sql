3ef1c4b58c8e3a3088650052fe89dc6a03a2d468
fabio-perezz <fabio_perez@hakkoda.io>
2022-04-07 09:54:27 -0600

    first test market incremental and unique key

--- models/dimensions/DIM_MARKET.sql
+++ models/dimensions/DIM_MARKET.sql
@@ -1,9 +1,12 @@
+{{ config(materialized='incremental', unique_key='DIM_MARKET_PK') }}
+
 WITH source -- the CTE view name
 	AS(
         SELECT
             {{ dbt_utils.surrogate_key(['WH_CD']) }} AS DIM_MARKET_PK
             , *
         FROM {{ ref('INT_DIV_CORP') }}
+        WHERE IS_ACT = 1
         ORDER BY DIM_MARKET_PK
     )
 SELECT * FROM source -- from the CTE view build a new reference with this filename
